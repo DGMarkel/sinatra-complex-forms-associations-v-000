484e1c0b75896658f2990af48b3172c83527ca99
David Markel <davidgmarkel@gmail.com>
2018-06-12 14:20:39 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -37,7 +37,7 @@ class PetsController < ApplicationController
     if !params["owner_name"].empty?
       @pet.owner = Owner.create(name: params["owner_name"])
     else
-      @owner =  Owner.find(params[:owner_id].to_i)
+      @pet.owner = Owner.find(params[:owner_id].to_i)    
     end
     if !params["pet_name"].empty?
       @pet.name = params["pet_name"]
